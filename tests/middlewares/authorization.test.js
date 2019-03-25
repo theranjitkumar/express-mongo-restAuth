0a81e6d20b90392eb02b7e067a2279aefebef42a
theranjitkumar <ranjit.kumar@mobileprogramming.net>
2019-03-25 20:25:19 +0530

    updated

--- tests/middlewares/authorization.test.js
+++ tests/middlewares/authorization.test.js
@@ -2,20 +2,11 @@ const expect = require('chai').expect;
 
 const { isAuthorized } = require('../../api/middlewares/authorization');
 
-// describe('isAuthorized Test', function() {
-//     describe('isAuthorized() function', function() {
-//         it('Should error if authorization failed ', function() {
-//             isAuthorized(req, res, next);
-//             expect(next);
-//         });
-//     })
-// });
-
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+describe('isAuthorized Test', function () {
+  describe('isAuthorized() function', function () {
+    it('Should error out if no ip logged ', function (done) {
+      isAuthorized
+      done();
     });
-  });
+  })
 });
